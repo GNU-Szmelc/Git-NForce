7d4de605782713e491b2384b302a9f358534cc31
[Silverainox] <serainox@gmail.com>
2023-04-04 10:05:45 +0200

    Update FSR2L.sh

--- FSR2L.sh
+++ FSR2L.sh
@@ -6,7 +6,7 @@ script_dir=$(dirname "$0")
 # Load the config file into a Bash variable
 config=$(jq -r '.' "$script_dir/config.json")
 
-# Load the data from the config.json file
+# Load the data from the config.yml file
 m_name=$(echo "$config" | jq -r '.main.name')
 m_repo=$(echo "$config" | jq -r '.main.repo')
 
@@ -20,7 +20,7 @@ fi
 # Obsidian CLI Szmelc.INC - GitHub Force Sync [Remote -> Local] [R2L]
 
 REMOTE="git@github.com:$m_name/$m_repo"
-LOCAL="./Obsidian" # Will create same folder name as repository
+LOCAL="./$m_repo" # Will create same folder name as repository
 
 echo $REMOTE
 # Initialize directory
@@ -31,4 +31,4 @@ fi
 # Purge local Obsidian directory, clone remote, remove .git folder.
 rm -fr "$LOCAL"
 git clone "$REMOTE"
-rm -fr "./Obsidian/.git"
+rm -fr "./$m_repo/.git"
